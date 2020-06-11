f34460cdd052518004c01e98a28969dce442d748
codeAl <codeal@junior.com>
2020-06-11 14:39:27 +0200

    handle initial data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import LoadingBar from "react-redux-loading";
+import { handleInitialData } from "../actions/shared";
 
 export class App extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(handleInitialData());
+  }
   render() {
     return (
       <div>
+        <LoadingBar />
         Would You Rather App
       </div>
-    )
+    );
   }
 }
 
-export default App
+function mapStateToProps({ users, questions }) {
+  return {
+    loading: users === {} && questions === {},
+  };
+}
+
+export default connect(mapStateToProps)(App);
